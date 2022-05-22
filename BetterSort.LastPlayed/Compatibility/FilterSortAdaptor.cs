7f4e4c15e6b5719b41bf82645f824355fffe22e0
nanikit <nanikit4@gmail.com>
2022-05-22 13:38:08 +0900

    feat: add timeout for sorting operation

--- BetterSort.LastPlayed/Compatibility/FilterSortAdaptor.cs
+++ BetterSort.LastPlayed/Compatibility/FilterSortAdaptor.cs
@@ -27,18 +27,28 @@ namespace BetterSort.LastPlayed.Compatibility {
     }
 
     public void ContextSwitch(SelectLevelCategoryViewController.LevelCategory levelCategory, IAnnotatedBeatmapLevelCollection? playlist) {
+      // It doesn't need this information.
     }
 
     public void DoSort(ref IEnumerable<IPreviewBeatmapLevel> levels, bool ascending) {
       _logger.Trace($"FilterSortAdaptor.DoSort({levels.Count()}, {ascending}) is called.");
       _result = new();
       _sorter.NotifyChange(levels.Select(level => new LevelPreview(level)), true);
-      _result.Task.Wait();
+
+      // Actually it sorts synchronously so sort is done already. Timeout is just for safety.
+      bool isComplete = _result.Task.Wait(1000);
+      if (!isComplete) {
+        _logger.Error($"Timeout exceeded. Current implementation doesn't support asynchonocity.");
+        return;
+      }
 
       var newLevels = _result.Task.Result?.Levels;
-      _logger.Trace($"FilterSortAdaptor.DoSort() newLevels[0]: {(newLevels?.Count() > 0 ? newLevels.First().SongName : "_empty")}");
       if (newLevels != null) {
+        _logger.Trace($"FilterSortAdaptor.DoSort() newLevels[0]: {(newLevels?.Count() > 0 ? newLevels.First().SongName : "_empty")}");
         levels = newLevels.OfType<LevelPreview>().Select(preview => preview.Preview).ToList();
+      }
+      else {
+        _logger.Info("Sorter doesn't support these maps. Skip.");
       }
     }
 
