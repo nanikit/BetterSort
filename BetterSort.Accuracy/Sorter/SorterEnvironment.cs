4c0bc6a62271f1903402dba73ee915b359542386
nanikit <nanikit4@gmail.com>
2024-02-24 08:34:48 +0900

    refactor: remove fields

--- BetterSort.Accuracy/Sorter/SorterEnvironment.cs
+++ BetterSort.Accuracy/Sorter/SorterEnvironment.cs
@@ -10,34 +10,28 @@ namespace BetterSort.Accuracy.Sorter {
   public class SorterEnvironment(SiraLog logger, IAccuracyRepository repository, IBsInterop bsInterop,
     FilterSortAdaptor adaptor, UnifiedImporter importer, ITransformerPluginHelper pluginHelper
   ) : IInitializable {
-    private readonly FilterSortAdaptor _adaptor = adaptor;
-    private readonly IBsInterop _bsInterop = bsInterop;
-    private readonly UnifiedImporter _importer = importer;
-    private readonly SiraLog _logger = logger;
-    private readonly IAccuracyRepository _repository = repository;
-    private readonly ITransformerPluginHelper _pluginHelper = pluginHelper;
 
     public async void Initialize() {
       try {
-        _bsInterop.OnSongPlayed += RecordHistoryWithGuard;
-        _logger.Info($"{nameof(Initialize)}.");
+        bsInterop.OnSongPlayed += RecordHistoryWithGuard;
+        logger.Info($"{nameof(Initialize)}.");
 
-        await _importer.CollectOrImport().ConfigureAwait(false);
-        _pluginHelper.Register(_adaptor);
+        await importer.CollectOrImport().ConfigureAwait(false);
+        pluginHelper.Register(adaptor);
       }
       catch (Exception ex) {
-        _logger.Error(ex);
+        logger.Error(ex);
       }
     }
 
     private async Task RecordHistory(PlayRecord record) {
-      var data = await _repository.Load().ConfigureAwait(false);
+      var data = await repository.Load().ConfigureAwait(false);
 
       var records = data ?? [];
       var (levelId, type, difficulty, accuracy) = record;
       AccuracyRepository.AddIfBest(records, new BestRecord(levelId, type, difficulty, accuracy));
 
-      await _repository.Save(records).ConfigureAwait(false);
+      await repository.Save(records).ConfigureAwait(false);
     }
 
     private async void RecordHistoryWithGuard(PlayRecord record) {
@@ -45,7 +39,7 @@ namespace BetterSort.Accuracy.Sorter {
         await RecordHistory(record).ConfigureAwait(false);
       }
       catch (Exception ex) {
-        _logger.Error(ex);
+        logger.Error(ex);
       }
     }
   }
