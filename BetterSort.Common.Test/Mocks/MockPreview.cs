0552e3be0cbc765426c3195ab7ac0de19f6cf80a
nanikit <nanikit4@gmail.com>
2024-08-25 22:03:36 +0900

    test: fix failure
    I don't know why test case filter does not work as expected.

--- BetterSort.Common.Test/Mocks/MockPreview.cs
+++ BetterSort.Common.Test/Mocks/MockPreview.cs
@@ -10,7 +10,7 @@ namespace BetterSort.Common.Test.Mocks {
 
     public static BaseBeatmapLevel GetMockPreviewBeatmapLevel(string id) {
 #if NOT_BEFORE_1_36_2
-      var mock = new Mock<BaseBeatmapLevel>([false, id, id, id, id, new string[] { }, new string[] { }, 0, 0, 0, 0, 0, 0, null, null, null]);
+      var mock = new Mock<BaseBeatmapLevel>([false, id, id, id, id, new string[] { }, new string[] { }, 0, 0, 0, 0, 0, 0, PlayerSensitivityFlag.Unknown, null, null]);
 #else
       var mock = new Mock<BaseBeatmapLevel>();
       mock.Setup(mock => mock.levelID).Returns(id);
